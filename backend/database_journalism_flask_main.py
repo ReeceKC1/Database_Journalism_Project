ea8bb8059fabd3ed37af798218b96a3c7248bba3
Reece Kemball-Cook <reecekc1@gmail.com>
2019-11-14 12:52:26 -0600

    fixed backend and reordered file names

--- backend/database_journalism_flask_main.py
+++ backend/database_journalism_flask_main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello"
+    return "goodbye"
---
+++ backend/start_script.sh
@@ -0,0 +1,3 @@
+source env/Scripts/activate
+export FLASK_APP=database_journalism_flask_main.py
+flask run
