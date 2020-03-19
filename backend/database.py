bc6d5459f5a070e8922505f28f0f45b2216c89ee
ReeceKC1 <reecekc1@gmail.com>
2020-03-19 14:18:47 -0500

    Changed string size

--- backend/database.py
+++ backend/database.py
@@ -17,14 +17,14 @@ Base = declarative_base()
 # Declaring tables with attributes
 class Student(Base):
     __tablename__ = 'student'
-    student_id = Column('student_id', String(255), unique = True, primary_key = True)
-    first_name = Column('first_name', String(255))
-    last_name = Column('last_name', String(255))
-    class_year = Column('class_year', String(255))
-    email = Column('email', String(255))
-    semester_of_completion = Column('semester_of_completion', String(255))
-    grade = Column('grade', String(255))
-    pr_major_minor = Column('pr_major_minor', String(255))
+    student_id = Column('student_id', String(100), unique = True, primary_key = True)
+    first_name = Column('first_name', String(100))
+    last_name = Column('last_name', String(100))
+    class_year = Column('class_year', String(100))
+    email = Column('email', String(100))
+    semester_of_completion = Column('semester_of_completion', String(100))
+    grade = Column('grade', String(100))
+    pr_major_minor = Column('pr_major_minor', String(100))
 
     @property
     def seralize(self):
@@ -41,9 +41,9 @@ class Student(Base):
 
 class Company(Base):
     __tablename__ = 'company'
-    company_name = Column('company_name', String(255), unique = True, primary_key = True)
-    address = Column('address', String(255))
-    phone = Column('phone', String(255))
+    company_name = Column('company_name', String(100), unique = True, primary_key = True)
+    address = Column('address', String(100))
+    phone = Column('phone', String(100))
 
     @property
     def seralize(self):
@@ -55,10 +55,10 @@ class Company(Base):
 
 class Supervisor(Base):
     __tablename__ = 'supervisor'
-    email = Column('email', String(255), unique = True, primary_key = True)
-    name = Column('name', String(255))
-    title = Column('title', String(255))
-    company_name = Column('company_name', String(255), ForeignKey('company.company_name'))
+    email = Column('email', String(100), unique = True, primary_key = True)
+    name = Column('name', String(100))
+    title = Column('title', String(100))
+    company_name = Column('company_name', String(100), ForeignKey('company.company_name'))
 
     @property
     def seralize(self):
@@ -72,11 +72,11 @@ class Supervisor(Base):
 
 class Internship(Base):
     __tablename__ = 'internship'
-    student_id = Column('student_id', String(255), ForeignKey('student.student_id'), primary_key = True)
-    company_name = Column('company_name', String(255), ForeignKey('company.company_name'), primary_key = True)
-    start_date = Column('start_date', String(255), primary_key = True)
-    end_date = Column('end_date', String(255))
-    supervisor_email = Column('supervisor_email', String(255), ForeignKey('supervisor.email'))
+    student_id = Column('student_id', String(100), ForeignKey('student.student_id'), primary_key = True)
+    company_name = Column('company_name', String(100), ForeignKey('company.company_name'), primary_key = True)
+    start_date = Column('start_date', String(100), primary_key = True)
+    end_date = Column('end_date', String(100))
+    supervisor_email = Column('supervisor_email', String(100), ForeignKey('supervisor.email'))
     hours = Column('hours', Float)
 
     @property
@@ -92,10 +92,10 @@ class Internship(Base):
 
 class Evaluation(Base):
     __tablename__ = 'evaluation'
-    year = Column('year', String(255), index = True, primary_key = True)
-    eval_type = Column('eval_type', String(255), index = True, primary_key = True)
-    title = Column('title', String(255))
-    version = Column('version', String(255))
+    year = Column('year', String(100), index = True, primary_key = True)
+    eval_type = Column('eval_type', String(100), index = True, primary_key = True)
+    title = Column('title', String(100))
+    version = Column('version', String(100))
 
     @property
     def seralize(self):
@@ -108,12 +108,12 @@ class Evaluation(Base):
 
 class Question(Base):
     __tablename__ = 'question'
-    evaluation_year = Column('evaluation_year', String(255), ForeignKey('evaluation.year'), primary_key = True)
-    evaluation_type = Column('evaluation_type', String(255), ForeignKey('evaluation.eval_type'), primary_key = True)
-    question_id = Column('question_id', String(255), index = True, unique = True, primary_key = True)
-    label = Column('label', String(255))
-    question_text = Column('question_text', String(255))
-    order_value = Column('order_value', String(255))
+    evaluation_year = Column('evaluation_year', String(100), ForeignKey('evaluation.year'), primary_key = True)
+    evaluation_type = Column('evaluation_type', String(100), ForeignKey('evaluation.eval_type'), primary_key = True)
+    question_id = Column('question_id', String(100), index = True, unique = True, primary_key = True)
+    label = Column('label', String(100))
+    question_text = Column('question_text', String(100))
+    order_value = Column('order_value', String(100))
 
     @property
     def seralize(self):
@@ -132,9 +132,9 @@ class Question(Base):
 
 class Option(Base):
     __tablename__ = 'option'
-    question_id = Column('question_id', String(255), ForeignKey('question.question_id'), primary_key = True)
-    option_text = Column('option_text', String(255), index = True, primary_key = True)
-    option_weight = Column('option_weight', String(255))
+    question_id = Column('question_id', String(100), ForeignKey('question.question_id'), primary_key = True)
+    option_text = Column('option_text', String(100), index = True, primary_key = True)
+    option_weight = Column('option_weight', String(100))
 
     @property
     def seralize(self):
@@ -146,8 +146,8 @@ class Option(Base):
 
 class Comment(Base):
     __tablename__ = 'comment'
-    comment_id = Column('comment_id', String(255), unique = True, primary_key = True)
-    comment_text = Column('comment_text', String(255))
+    comment_id = Column('comment_id', String(100), unique = True, primary_key = True)
+    comment_text = Column('comment_text', String(100))
 
     @property
     def seralize(self):
@@ -158,13 +158,13 @@ class Comment(Base):
 
 class Evaluation_Answers(Base):
     __tablename__ = 'evaluation_answers'
-    eval_type = Column('eval_type', String(255), ForeignKey('evaluation.eval_type'), primary_key = True)
-    eval_year = Column('eval_year', String(255), ForeignKey('evaluation.year'), primary_key = True)
-    student_id = Column('student_id', String(255), ForeignKey('student.student_id'), primary_key = True)
-    company_name = Column('company_name', String(255), ForeignKey('company.company_name'), primary_key = True)
-    supervisor_email = Column('supervisor_email', String(255), ForeignKey('supervisor.email'), primary_key = True)
-    answer_id = Column('answer_id', String(255), index = True, unique = True, primary_key = True)
-    comment_id = Column('comment_id', String(255), ForeignKey('comment.comment_id'))
+    eval_type = Column('eval_type', String(100), ForeignKey('evaluation.eval_type'), primary_key = True)
+    eval_year = Column('eval_year', String(100), ForeignKey('evaluation.year'), primary_key = True)
+    student_id = Column('student_id', String(100), ForeignKey('student.student_id'), primary_key = True)
+    company_name = Column('company_name', String(100), ForeignKey('company.company_name'), primary_key = True)
+    supervisor_email = Column('supervisor_email', String(100), ForeignKey('supervisor.email'), primary_key = True)
+    answer_id = Column('answer_id', String(100), index = True, unique = True, primary_key = True)
+    comment_id = Column('comment_id', String(100), ForeignKey('comment.comment_id'))
 
     @property
     def seralize(self):
@@ -180,11 +180,11 @@ class Evaluation_Answers(Base):
 
 class Portfolio_Answers(Base):
     __tablename__ = 'portfolio_answers'
-    eval_type = Column('eval_type', String(255), ForeignKey('evaluation.eval_type'), primary_key = True)
-    eval_year = Column('eval_year', String(255), ForeignKey('evaluation.year'), primary_key = True)
-    student_id = Column('student_id', String(255), ForeignKey('student.student_id'), primary_key = True)
-    reviewer_name = Column('reviewer_name', String(255), primary_key = True)
-    answer_id = Column('answer_id', String(255), index = True, unique = True, primary_key = True)
+    eval_type = Column('eval_type', String(100), ForeignKey('evaluation.eval_type'), primary_key = True)
+    eval_year = Column('eval_year', String(100), ForeignKey('evaluation.year'), primary_key = True)
+    student_id = Column('student_id', String(100), ForeignKey('student.student_id'), primary_key = True)
+    reviewer_name = Column('reviewer_name', String(100), primary_key = True)
+    answer_id = Column('answer_id', String(100), index = True, unique = True, primary_key = True)
     date_reviewed = Column('date_reviewed', Date, primary_key = True)
 
     @property
@@ -200,9 +200,9 @@ class Portfolio_Answers(Base):
 
 class Eval_Answer(Base):
     __tablename__ = 'eval_answer'
-    answer_id = Column('answer_id', String(255), ForeignKey('evaluation_answers.answer_id'), primary_key = True)
-    option_text = Column('option_text', String(255), ForeignKey('option.option_text'), primary_key = True)
-    question_id = Column('question_id', String(255), ForeignKey('question.question_id'), primary_key = True)
+    answer_id = Column('answer_id', String(100), ForeignKey('evaluation_answers.answer_id'), primary_key = True)
+    option_text = Column('option_text', String(100), ForeignKey('option.option_text'), primary_key = True)
+    question_id = Column('question_id', String(100), ForeignKey('question.question_id'), primary_key = True)
 
     @property
     def seralize(self):
@@ -214,10 +214,10 @@ class Eval_Answer(Base):
 
 class Port_Answer(Base):
     __tablename__ = 'port_answer'
-    answer_id = Column('answer_id', String(255), ForeignKey('portfolio_answers.answer_id'), primary_key = True)
-    option_text = Column('option_text', String(255), ForeignKey('option.option_text'), primary_key = True)
-    question_id = Column('question_id', String(255), ForeignKey('question.question_id'), primary_key = True)
-    comment_id =Column('comment_id', String(255), ForeignKey('comment.comment_id'))
+    answer_id = Column('answer_id', String(100), ForeignKey('portfolio_answers.answer_id'), primary_key = True)
+    option_text = Column('option_text', String(100), ForeignKey('option.option_text'), primary_key = True)
+    question_id = Column('question_id', String(100), ForeignKey('question.question_id'), primary_key = True)
+    comment_id =Column('comment_id', String(100), ForeignKey('comment.comment_id'))
 
     @property
     def seralize(self):
